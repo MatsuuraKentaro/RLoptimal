4281d03acd44ff9958f8ad6504b91a810cad2d37
hoxo-m <hoxo.smile@gmail.com>
2024-07-21 06:36:37 +0900

    modify the Ray version check to occur only once for acceleration

--- R/utils.R
+++ R/utils.R
@@ -11,9 +11,17 @@ softmax <- function(x) {
   return(exp_x / sum(exp_x))
 }
 
+.ray_version <- NULL
+
 ray_version <- function() {
-  list_packages <- reticulate::py_list_packages()
-  ray_version <- list_packages[list_packages$package == "ray", "version"]
-  if (length(ray_version) == 0L) stop("Ray is not found.")
-  ray_version
+  if (is.null(.ray_version)) {
+    list_packages <- reticulate::py_list_packages()
+    ray_version <- list_packages[list_packages$package == "ray", "version"]
+    if (length(ray_version) == 0L) stop("Ray is not found.")
+    package_namespace <- asNamespace(.packageName)
+    unlockBinding(".ray_version", package_namespace)
+    assign(".ray_version", ray_version, envir = package_namespace)
+    lockBinding(".ray_version", package_namespace)
+  }
+  .ray_version
 }
