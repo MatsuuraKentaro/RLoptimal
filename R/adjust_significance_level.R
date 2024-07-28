4701600171b3678fa0e29f9d9a54d6a02ee4ef06
Kentaro Matsuura <matsuurakentaro55@gmail.com>
2024-07-28 17:24:35 +0900

    add high level functions to evaluate an obtained allocation rule

---
+++ R/adjust_significance_level.R
@@ -0,0 +1,48 @@
+#' Adjust Significance Level on a Simulation Basis 
+#'
+#' @param allocation_rule An object of class \link[RLoptimal]{AllocationRule}
+#'        specifying an obtained optimal adaptive allocation rule.
+#' @param models An object of class \link[DoseFinding]{Mods} specifying assumed
+#'        dose-response models. This is used in the MCPMod method at the end of 
+#'        this study.
+#' @param N_total A positive integer value. The total number of subjects.
+#' @param N_ini A positive integer vector in which each element is greater than 
+#'        or equal to 2. The number of subjects initially assigned to each dose. 
+#' @param N_block A positive integer value. The number of subjects allocated
+#'        adaptively in each round.
+#' @param sd_normal A positive numeric value. The standard deviation of the
+#'        observation noise.
+#' @param alpha A positive numeric value. The original significance level.
+#'        Default is 0.025.
+#' @param n_sim A positive integer value. The number of simulation studies
+#'        to calculate the adjusted significance level. Default is 10000.
+#' @param seed An integer value. Random seed for data generation in the simulation
+#'        studies.
+#'        
+#' @returns A positive numeric value specifying adjusted significance level.
+#'
+#' @export
+adjust_significance_level <- function(
+    allocation_rule, models,
+    N_total, N_ini, N_block, sd_normal, 
+    # type = c("normal", "general"),
+    alpha = 0.025, n_sim = 10000L, seed = NULL) {
+  
+  if (!is.null(seed)) {
+    seed <- 0
+  }
+  
+  doses <- attr(models, "doses")
+  K <- length(doses)
+  true_response <- rep(attr(models, "placEff"), K)
+  
+  p_values <- sapply(seq_len(n_sim), function(simID) {
+    res <- simulate_one_study(
+      allocation_rule, models, "flat", true_response,
+      N_total, N_ini, N_block, NULL, sd_normal, alpha, NULL, 
+      simID + seed, "pVal")
+    res$min_p_value
+  })
+  adjusted <- unname(quantile(p_values, prob = alpha))
+  adjusted
+}
---
+++ R/simulate_one_study.R
@@ -0,0 +1,82 @@
+#' Simulate One Study Using an Obtained Optimal Adaptive Allocation Rule
+#'
+#' @param allocation_rule An object of class \link[RLoptimal]{AllocationRule}
+#'        specifying an obtained optimal adaptive allocation rule.
+#' @param models An object of class \link[DoseFinding]{Mods} specifying assumed
+#'        dose-response models. This is used in the MCPMod method at the end of 
+#'        this study.
+#' @param true_model_name A character value specifying the true model name.
+#' @param true_response A numeric vector specifying the true response values of 
+#'        the true model.
+#' @param N_total A positive integer value. The total number of subjects.
+#' @param N_ini A positive integer vector in which each element is greater than 
+#'        or equal to 2. The number of subjects initially assigned to each dose. 
+#' @param N_block A positive integer value. The number of subjects allocated
+#'        adaptively in each round.
+#' @param Delta A positive numeric value. The clinically relevant target effect.
+#'        See \link[DoseFinding]{TD} for details.
+#' @param sd_normal A positive numeric value. The standard deviation of the
+#'        observation noise.
+#' @param alpha A positive numeric value. The significance level. Default is 0.025.
+#' @param selModel A character value specifying the model selection criterion
+#'        for dose estimation. Possible values are "AIC" (default), "maxT", or
+#'        "aveAIC". See \link[DoseFinding]{MCPMod} for details.
+#' @param seed An integer value. Random seed for data generation in this study.
+#' @param eval_type A character value specifying the evaluation type. Possible 
+#'        values are "all" (default) and "pVal". "all" returns all metrics,
+#'        which contain the minimum p value, the selected model name, 
+#'        the estimated target dose, and the MAE. "pVal" returns only the 
+#'        minimum p value without fitting models.
+#'        
+#' @returns A list which contains the minimum p value, the selected model name, 
+#'        the estimated target dose, and the MAE.
+#'
+#' @export
+simulate_one_study <- function(
+    allocation_rule, models, true_model_name, true_response, 
+    N_total, N_ini, N_block, Delta, sd_normal,
+    # type = c("normal", "general"),
+    alpha = 0.025, selModel = c("AIC", "maxT", "aveAIC"), 
+    seed = NULL, eval_type = c("all", "pVal")) {
+  
+  if (!is.null(seed)) {
+    set.seed(seed)
+  }
+  
+  doses <- attr(models, "doses")
+  K <- length(doses)
+  actions <- seq_len(K)
+  
+  sim_actions <- rep(actions, times = N_ini)
+  sim_doses <- doses[sim_actions]
+  sim_resps <- rnorm(sum(N_ini), mean = true_response[sim_actions], sd = sd_normal)
+  
+  while (length(sim_resps) < N_total) {
+    probs <- allocation_rule$get_next_action_probs(sim_doses, sim_resps)
+    ns <- DoseFinding::rndDesign(probs, N_block)
+    new_actions <- rep(actions, times = ns)
+    new_doses <- doses[new_actions]
+    new_resps <- rnorm(N_block, mean = true_response[new_actions], sd = sd_normal)
+    sim_actions <- c(sim_actions, new_actions)
+    sim_doses <- c(sim_doses, new_doses)
+    sim_resps <- c(sim_resps, new_resps)
+  }
+  
+  if (eval_type == "pVal") {
+    result_mct <- DoseFinding::MCTtest(
+      dose = sim_doses, resp = sim_resps, models = models,
+      alpha = alpha, pVal = TRUE, alternative = "one.sided")
+    p_values <- attr(result_mct$tStat, "pVal")
+    min_p_value <- min(p_values)
+    return(list(min_p_value = min_p_value))
+  }
+  
+  compute_metric_base <- local({
+    eval(allocation_rule$info$setup_code)
+    compute_metric_base
+  })
+  
+  # To Do: pass models, Delta, alpha, and selModel
+  metrics <- compute_metric_base(true_model_name, true_response, sim_doses, sim_resps)
+  metrics  
+}
