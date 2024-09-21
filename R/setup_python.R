e9b89e2078de9b60d1a476d22164c9af8c470ca5
hoxo-m <hoxo.smile@gmail.com>
2024-09-21 17:15:26 +0900

    fix the versions of python packages
    "dm-tree==0.1.8",
    "lz4==4.3.3",
    "pyarrow==17.0.0",
    "gymnasium==0.29.1",
    "torch==2.4.1",
    "scikit-image==0.24.0",
    "pandas==2.2.3",
    "typer==0.12.5",
    "ray==2.36.0"

--- R/setup_python.R
+++ R/setup_python.R
@@ -8,14 +8,17 @@ virtualenv_name <- "RLoptimal"
 #' @export
 setup_python <- function() {
   py_version <- "3.11"
-  required_packages <- c("numpy", "dm-tree", "lz4", "pyarrow", "gymnasium", "torch",
-                         "scikit-image", "pandas", "typer", "ray")
+  required_packages <- c(
+    # "numpy>=2.1.1", 
+    "dm-tree==0.1.8", "lz4==4.3.3", "pyarrow==17.0.0",
+    "gymnasium==0.29.1", "torch==2.4.1", "scikit-image==0.24.0", 
+    "pandas==2.2.3", "typer==0.12.5", "ray==2.36.0")
 
   if (is.null(reticulate::virtualenv_starter(py_version))) {
     reticulate::install_python(py_version)
   }
   if (reticulate::virtualenv_exists(virtualenv_name)) {
-    installed_packages <- reticulate::py_list_packages(virtualenv_name)$package
+    installed_packages <- reticulate::py_list_packages(virtualenv_name)$requirement
     required_packages <- setdiff(required_packages, installed_packages)
   } else {
     reticulate::virtualenv_create(virtualenv_name, version = py_version)
