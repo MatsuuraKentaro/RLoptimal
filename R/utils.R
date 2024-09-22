38de6dd250bdb000cb794b0190e93e2d94e76234
hoxo-m <hoxo.smile@gmail.com>
2024-09-22 17:06:28 +0900

    remove ray_version()

--- R/utils.R
+++ R/utils.R
@@ -17,18 +17,3 @@ compute_MAE <- function(estimated_response, true_response) {
   errors <- shifted_estimates[-1L] - shifted_true[-1L]
   return(mean(abs(errors)))
 }
-
-.ray_version <- NULL
-
-ray_version <- function() {
-  if (is.null(.ray_version)) {
-    list_packages <- reticulate::py_list_packages()
-    ray_version <- list_packages[list_packages$package == "ray", "version"]
-    if (length(ray_version) == 0L) stop("Ray is not found.")
-    package_namespace <- asNamespace(.packageName)
-    unlockBinding(".ray_version", package_namespace)
-    assign(".ray_version", ray_version, envir = package_namespace)
-    lockBinding(".ray_version", package_namespace)
-  }
-  .ray_version
-}
