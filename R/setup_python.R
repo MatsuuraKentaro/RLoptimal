646b6444eca75dbe93328f245719e2396cbfea7f
hoxo-m <hoxo.smile@gmail.com>
2024-09-27 02:34:40 +0900

    change Python virtual environment name

--- R/setup_python.R
+++ R/setup_python.R
@@ -1,12 +1,12 @@
-virtualenv_name <- "RLoptimal"
-
 #' Setting up a Python Virtual Environment
 #'
 #' Setting up a Python virtual environment for the Ray package, which includes
 #' the RLlib library for reinforcement learning.
+#' 
+#' @param envname Python virtual environment name.
 #'
 #' @export
-setup_python <- function() {
+setup_python <- function(envname = "r-RLoptimal") {
   py_version <- "3.11"
   required_packages <- c(
     # "numpy>=2.1.1", 
@@ -17,26 +17,28 @@ setup_python <- function() {
   if (is.null(reticulate::virtualenv_starter(py_version))) {
     reticulate::install_python(py_version)
   }
-  if (reticulate::virtualenv_exists(virtualenv_name)) {
-    installed_packages <- reticulate::py_list_packages(virtualenv_name)$requirement
+  if (reticulate::virtualenv_exists(envname)) {
+    installed_packages <- reticulate::py_list_packages(envname)$requirement
     required_packages <- setdiff(required_packages, installed_packages)
   } else {
-    reticulate::virtualenv_create(virtualenv_name, version = py_version)
+    reticulate::virtualenv_create(envname, version = py_version)
   }
   if (length(required_packages) > 0) {
     py_install <- function(package) {
-      reticulate::py_install(package, virtualenv_name)
+      reticulate::py_install(package, envname)
     }
     Map(py_install, required_packages)
     message("The setup process for Python is complete.")
   }
-  reticulate::use_virtualenv(virtualenv_name)
+  reticulate::use_virtualenv(envname)
   invisible(NULL)
 }
 
 #' Clean the Python Virtual Environment
 #' 
+#' @param envname Python virtual environment name.
+#' 
 #' @export
-clean_python_settings <- function() {
-  reticulate::virtualenv_remove(virtualenv_name, confirm = TRUE)
+clean_python_settings <- function(envname = "r-RLoptimal") {
+  reticulate::virtualenv_remove(envname, confirm = TRUE)
 }
