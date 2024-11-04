20c645fef6d53284d43608acdc3f494ab1bb69d7
hoxo-m <hoxo.smile@gmail.com>
2024-11-04 15:14:38 +0900

    add tests for allocation_rule.R

---
+++ data-raw/generate_allocation_rule_files_for_test.R
@@ -0,0 +1,16 @@
+library(RLoptimal)
+
+doses <- c(0, 2, 4, 6, 8)
+
+models <- DoseFinding::Mods(
+  doses = doses, maxEff = 1.65,
+  linear = NULL, emax = 0.79, sigEmax = c(4, 5)
+)
+
+allocation_rule <- learn_allocation_rule(
+  models, N_total = 150, N_ini = rep(10, 5), N_block = 10, Delta = 1.3,
+  outcome_type = "continuous", sd_normal = sqrt(4.5), seed = 314, 
+  rl_config = rl_config_set(iter = 1, model = rl_dnn_config(fcnet_hiddens = c(1, 1))),
+  output_dir = "for_test", output_base_dir = "inst/allocation_rules",
+  alpha = 0.025
+)
---
+++ tests/testthat/test-allocation_rule.R
@@ -0,0 +1,47 @@
+file_name_for_test <- "for_test"
+dirpath_for_test <- system.file("allocation_rules", package = "RLoptimal")
+filepath_for_test <- system.file("allocation_rules/for_test", package = "RLoptimal")
+
+test_that("generate object", {
+  obj <- AllocationRule$new(dir = filepath_for_test)
+  expect_equal(class(obj), c("AllocationRule", "R6"))
+})
+
+test_that("base_dir", {
+  obj <- AllocationRule$new(dir = file_name_for_test, base_dir = dirpath_for_test)
+  expect_equal(class(obj), c("AllocationRule", "R6"))
+})
+
+obj <- AllocationRule$new(dir = filepath_for_test)
+
+test_that("opt_allocation_probs", {
+  doses <- c( 0,  0,  0,  0,  2,  2,  4,  4,  4,  6,  6,   8,  8,   8)
+  resps <- c(.2, .1, .0, .3, .2, .4, .1, .6, .8, .5, .8, 1.1, .9, 1.6)
+  
+  expected <- c(`0` = 0.2036620, `2` = 0.1981888, `4` = 0.1986541, 
+                `6` = 0.1962135, `8` = 0.2032815)
+  act <- obj$opt_allocation_probs(doses, resps)
+  
+  expect_equal(expected, act, tolerance = 1e-6)
+})
+
+test_that("opt_allocation_probs - lengths of doses and resps do not match", {
+  doses <- c( 0,  0,  0,  0,  2,  2,  4,  4,  4,  6,  6,   8,  8,   8)
+  resps <- c(.2, .1, .0, .3, .2, .4, .1, .6, .8, .5, .8, 1.1, .9)
+  
+  expect_error(obj$opt_allocation_probs(doses, resps))
+})
+
+test_that("opt_allocation_probs - dose that was not present in training", {
+  doses <- c( 0,  0,  0,  1,  2,  2,  4,  4,  4,  6,  6,   8,  8,   8)
+  resps <- c(.2, .1, .0, .3, .2, .4, .1, .6, .8, .5, .8, 1.1, .9, 1.6)
+  
+  expect_error(obj$opt_allocation_probs(doses, resps))
+})
+
+test_that("opt_allocation_probs - count per action is less than 2", {
+  doses <- c( 0)
+  resps <- c(.2)
+  
+  expect_error(obj$opt_allocation_probs(doses, resps))
+})
